288dfb52df807c7e4130e1c3142f0bdc90e480d6
Ryan Sparks <ryan@ryan-sparks.com>
2017-07-21 02:07:44 -0700

    added test widgets to admin panel

--- admin/adminpanel.php
+++ admin/adminpanel.php
@@ -8,6 +8,48 @@ $rank = $_SESSION['rank'];
 <head>
 	<title>Admin Panel</title>
 	<link rel="stylesheet" type="text/css" href="../main.css">
+	<style type="text/css">
+		.widget {
+			display:inline-block;
+			width:300px;
+			border:1px solid green;
+			padding: 20px;
+			text-align:center;
+			height:200px;
+		}
+		.widget span {
+			font-family:"Courier New";
+			font-weight:bold;
+		}
+		.widget button {
+			-webkit-border-radius: 7;
+  			-moz-border-radius: 7;
+  			border-radius: 7px;
+  			font-family: Arial;
+  			color: #ffffff;
+  			font-size: 17px;
+  			padding: 7px 20px 7px 20px;
+  			text-decoration: none;
+  			width:100%;
+		}
+
+		.widget .positive {
+  			background: #019B0E;
+		}
+
+		.widget .positive:hover {
+			background:#007A0A;
+			text-decoration:none;
+		}
+
+		.widget .negative {
+			background:#A30000;
+		}
+
+		.widget .negative:hover {
+			background:#7F0000;
+		}
+	</style>
 </head>
 
 <body>
@@ -21,13 +63,29 @@ $rank = $_SESSION['rank'];
 	?>
 
 	<div class="content">
-	<p>
 	<?php
 	if($rank == "admin") {
 	?>
 		<!--If is logged in as admin-->
-			<div id="usermanagement">
-				User management
+			<div class="widget">
+				<span>Admin User Management</span>
+				<button type="button" class="positive" onclick="addUser()">Add admin user</button><br />
+				<button type="button" class="negative" onclick="removeUser()">Remove admin user</button><br />
+				<button type="button" class="positive" onclick="changeUserRank()">Change user Rank</button><br />
+			</div>
+			<div class="widget">
+				<span>Directory Management</span>
+				<button type="button" class="positive" onclick="addBusiness()">Manually Add Business</button><br />
+				<button type="button" class="positive" onclick="addBanner()">Add Banner Advertisement</button><br />
+				<button type="button" class="negative" onclick="removeBusiness()">Remove Business</button><br />
+				<button type="button" class="negative" onclick="removeBanner()">Remove Banner Advertisement</button><br />
+			</div>
+			<div class="widget">
+				<span>Directory Management</span>
+				<button type="button" class="positive" onclick="addBusiness()">Manually Add Business</button><br />
+				<button type="button" class="positive" onclick="addBanner()">Add Banner Advertisement</button><br />
+				<button type="button" class="negative" onclick="removeBusiness()">Remove Business</button><br />
+				<button type="button" class="negative" onclick="removeBanner()">Remove Banner Advertisement</button><br />
 			</div>
 	<?php
 	} else {
@@ -39,7 +97,6 @@ $rank = $_SESSION['rank'];
 	?>
 	<br /><br />
 	<a href="logout.php">Logout</a>
-	</p>
 	</div>
 
 	<div class="footer">
