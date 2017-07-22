408722ca109646a13a601d1d0e737e0a8519eb39
Ryan Sparks <ryan@ryan-sparks.com>
2017-07-22 06:35:34 -0700

    getlisted now adds values to a pending sql table, and breifs the customer on what to expect.

--- createbusinessaccount.php
+++ createbusinessaccount.php
@@ -1,5 +1,17 @@
+<html>
+<head>
+	<title>Create Business Account</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+	<?php include("header.php"); ?>
+
+	<div class="content">
+	<p>
+
 <?php
-include('connect.php');
 
 $companyname = $_POST["companyname"];
 $merchaddress = $_POST["merchaddress"];
@@ -11,14 +23,29 @@ $phone = $_POST["phone"];
 $email = $_POST["email"];
 $service = $_POST["service"];
 
-$sql = "INSERT INTO `pending_businesses`(`name`, `address`, `url`, `description`, `pcfirst`, `pclast`, `phone`, `email`, `servicetype`, `paid`, `datesubmitted`, `additional`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, '0', NOW(), '')";
-$stmt = $conn->prepare($sql);
+if($companyname!='' && $merchaddress!='' && $url!='' && $pcfirst!='' && $pclast!='' && $phone!='' && $email !='' && $service!='') {
+	include('connect.php');
+	$sql = "INSERT INTO `pending_businesses`(`name`, `address`, `url`, `description`, `pcfirst`, `pclast`, `phone`, `email`, `servicetype`, `paid`, `datesubmitted`, `additional`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, '0', NOW(), '')";
+	$stmt = $conn->prepare($sql);
 
-$stmt->bind_param("sssssssss", $companyname, $merchaddress, $url, $description, $pcfirst, $pclast, $phone, $email, $service);
+	$stmt->bind_param("sssssssss", $companyname, $merchaddress, $url, $description, $pcfirst, $pclast, $phone, $email, $service);
 
-$stmt->execute();
-$result = $stmt->get_result();
+	$stmt->execute();
+	$result = $stmt->get_result();
 
-echo("success");
-
+	echo("Thanks for creating an account with us! For your records, please keep this reference number: " . "A924" . ".  As soon as we receive payment, your business will become listed on our site.  Please note that if you've requested banner advertisements, a customer service representative will reach out with 1-2 business days to aquire the ad and post it on the requested portion of our site.  Thanks for your business!");
+} else {
+	echo("Please <a href='getlisted.php'>go back</a> and complete all parts of the form in order to create a business account with us. ");
+}
 ?>
+
+	</p>
+	</div>
+
+	<div class="footer">
+	<?php include("footer.php"); ?>
+	</div>
+</body>
+</html>
+<?php
+include('connect.php');
