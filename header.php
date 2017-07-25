59db12013a457e647fce409dacb0307b18dba3ad
Ryan Sparks <ryan@ryan-sparks.com>
2017-07-25 13:02:29 -0700

    Fixed merge conflict

--- header.php
+++ header.php
@@ -6,6 +6,5 @@
 			<li><a href="locations.php">Directory</a></li>
 			<li><a href="photos.php">Photos</a></li>
 			<li><a href="planit.php">Plan it!</a></li>
-			<li><a href="#">Plan it!</a></li>
 	</ul></nav>
 </div>
